c5012fd865b60aabc3f7c3e5e5d5ebbaeb11b8d2
Andrey Klochok <andreyklochok@gmail.com>
2019-05-29 19:26:41 +0300

    Added styles

--- src/AdvancedhostingAsset.php
+++ src/AdvancedhostingAsset.php
@@ -10,11 +10,36 @@
 
 namespace advancedhosters\assets\advancedhosting;
 
-class AdvancedhostingAsset extends \yii\web\AssetBundle
+use yii\web\AssetBundle;
+
+class AdvancedhostingAsset extends AssetBundle
 {
     public $sourcePath = '@advancedhosters/assets/advancedhosting/assets';
 
     public $css = [
+        'https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
         'css/advancedhosting.css',
+    ];
+
+    public $js = [
+        // Popper JS
+        [
+            'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+            'integrity' => 'sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1',
+        ],
+        // Bootstrap 4 JS
+        [
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
+            'sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM',
+        ],
+    ];
+
+    public $cssOptions = [
+        'crossorigin' => 'anonymous',
+    ];
+
+    public $jsOptions = [
+        'crossorigin' => 'anonymous',
     ];
 }
